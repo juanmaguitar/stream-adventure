70d29275e3fd7b956894ffa44a7c2057bae24b7f
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-08-03 10:27:38 +0200

    TRANSFORM

--- program.js
+++ program.js
@@ -1,1 +1,16 @@
-process.stdin.pipe(process.stdout);
+var through = require('through2');
+var upperCaseProcess = through(write, end);
+
+function write (buffer, encoding, next) {
+	var strData = buffer.toString();
+	this.push( strData.toUpperCase() );
+	next();
+}
+
+function end (done) {
+	done();
+}
+
+process.stdin
+	.pipe( upperCaseProcess )
+	.pipe(process.stdout);
